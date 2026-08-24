57fb39f59f29b1c4894d41a4d4db98fa8f385f14
Кулаков Артём Максимович <127825138+ArtScienceMK@users.noreply.github.com>
2026-08-24 16:15:44 +0000

    Create QuadraticSolver(copy).h

---
+++ QuadraticSolver(copy).h
@@ -0,0 +1,22 @@
+#ifndef QUADRATIC_SOLVER_H
+#define QUADRATIC_SOLVER_H
+
+const int QUADRATIC_SOLVER_ERROR = -1;
+
+
+enum CNT_ROOTS {
+    ZERO_ROOTS = 0,
+    ONE_ROOT = 1,
+    TWO_ROOTS = 2,
+    INF_ROOTS = 8
+};
+
+struct equationCoefs {
+    double a, b, c;
+    int cntRoot;
+    double eqRoot1, eqRoot2;
+};
+
+int QuadraticSolver(const equationCoefs* const coefs, double* eqRoot1, double* eqRoot2);
+
+#endif /* QUADRATIC_SOLVER_H */
